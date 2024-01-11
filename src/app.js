6344b9174b580932bc370d03a0edbc81be2428b1
Manuel <leonmanu@gmail.com>
2024-01-10 22:19:54 -0300

    resubida de proyecto

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose')
 require("dotenv").config()
 
-// mongoose.connect(
-//   process.env.MONGODB_URI
-// )
-//   .then(
-//     ()=> console.log('Conectado!!')
-//   )
-//   .catch((err) => console.error(err))
+
 
 const express = require('express')
 
